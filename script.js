9b35309c76ecf6f5ca83e98d2060838cca5c337a
Sannu Kumar Ojha <137702131+SannuKumarOjha@users.noreply.github.com>
2023-07-25 17:19:28 +0530

    HTML , CSS , JavaScript
    HTML , CSS , JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const openSidebar = () =>{
+   document.getElementById('firstSide').style.left="0" ;
+  // alert("Request:- Open the sidebar");
+
+}
+
+const closeBtn = () =>{
+   document.getElementById('firstSide').style.left="-100%" ;
+  // alert("Request:- Close the sidebar");
+}
